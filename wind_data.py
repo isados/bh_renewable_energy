f95bb49776e92e897eae322f18fe252ad3254db2
Isa AlDoseri <it@petarabia.com>
2023-09-23 16:56:43 +0300

    add wind data

--- wind_data.py
+++ wind_data.py
@@ -30,68 +30,16 @@ filename = 'data/wind_daily_2022.pdf'
 assert os.path.exists(filename)
 
 
-pd.DataFrame(data=turn_into_text(extract(filename))).to_csv('out.csv', index=False)
+# pd.DataFrame(data=turn_into_text(extract(filename))).to_csv('out.csv', index=False)
 
-# month = 0
-# result = result[10:]
-
-# data = {}
-
-# for index, column in enumerate(result):
-#     if len(column) >=28:
-#         column = list(filter(lambda x: x, column.split('\n')))
-#         try:
-#             column = list(map(lambda x: Decimal(x.replace(',', '').replace(' ', '').replace(' ', '')), column))
-#         except InvalidOperation:
-#             # print(column)
-#             continue
-#         print(mean(column))
-#         column_index = chr(ord('a') + month)
-#         print('Index', column_index)
-#         data[column_index] = column
-#         month +=1
-#         # print(column[0])
-#         # print(column[-1])
-#         # print(len(column))
-#         # data = 
-#         print()
-
-# # print(data)
-# months_alpha_to_num_indices = {
-#     'a': 1,
-#     'b': 2,
-#     'c': 3,
-#     'd': 4,
-#     'e': 5,
-#     'f': 6,
-#     'g': 7,
-#     'h': 8,
-#     'i': 9,
-#     'j': 10,
-#     'l': 11,
-#     'k': 12,
-#     }
-
-# data = {
-#     months_alpha_to_num_indices[key]: data for key,data in data.items()
-# }
-
-# value_data = []
-# date_data = []
-
-# for month, data in data.items():
-#     date_data += [date(2022, month, day) for day in range(1, len(data)+1)]
-#     value_data += data
+# %%
+import numpy as np
+df = pd.read_csv('data/wind_daily_2022_intermediate.csv', na_values='')
+wind = df.melt(['Day'], var_name='month').replace(np.NaN, None).dropna()
+wind['date'] = wind.apply(lambda x: date(2022, int(x['month']), x['Day']), axis=1)
+wind = wind[['date', 'value']]
+wind.to_csv('data/wind_daily_2022.csv', index=False)
+wind
 
 
-# assert len(value_data) == len(date_data)
-# df = pd.DataFrame({'date': date_data, 'value': value_data})
-# df.to_csv('data/solar_daily_2022.csv', index=False)
-# df
-
-
-
-# # %%
-# chr(ord('a') + 1)
-
-    
+# %%
